e2065a8e6ca7e240dfbc55913402fb7b04c5be7d
Seob <yeongseob.dev@gmail.com>
2024-03-23 23:01:47 +0900

    update docusaurus v2 to v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,45 +1,46 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-require('dotenv').config();
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+require("dotenv").config();
+const { themes } = require("prism-react-renderer");
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Seob'log",
-  tagline: 'Be the type of person you want to meet',
-  url: 'https://seobie.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'seobie', // Usually your GitHub org/user name.
-  projectName: 'seobie.github.io', // Usually your repo name.
-  deploymentBranch: 'gp-pages',
+  tagline: "Be the type of person you want to meet",
+  url: "https://seobie.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "seobie", // Usually your GitHub org/user name.
+  projectName: "seobie.github.io", // Usually your repo name.
+  deploymentBranch: "gp-pages",
   trailingSlash: false,
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/seobie/seobie.github.io/tree/main/',
+          editUrl: "https://github.com/seobie/seobie.github.io/tree/main/",
         },
         blog: {
-          blogTitle: 'Blog',
+          blogTitle: "Blog",
           blogDescription:
-            '프론트엔드 개발을 하며 겪었던 문제나 스터디한 내용을 공유합니다. 사실은 아무거나 막올림',
+            "프론트엔드 개발을 하며 겪었던 문제나 스터디한 내용을 공유합니다. 사실은 아무거나 막올림",
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/seobie/seobie.github.io/tree/main/',
-          blogSidebarTitle: 'All posts',
-          blogSidebarCount: 'ALL',
+          editUrl: "https://github.com/seobie/seobie.github.io/tree/main/",
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
           postsPerPage: 5,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -51,8 +52,8 @@ const config = {
       navbar: {
         title: "Seob'log",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/avatar.png',
+          alt: "My Site Logo",
+          src: "img/avatar.png",
         },
         items: [
           // {
@@ -61,25 +62,25 @@ const config = {
           //   docId: 'intro',
           //   position: 'left',
           // },
-          { label: 'Blog', to: '/blog', position: 'left' },
-          { label: 'Algorithms', to: '/algorithms', position: 'left' },
+          { label: "Blog", to: "/blog", position: "left" },
+          { label: "Algorithms", to: "/algorithms", position: "left" },
           // { label: 'Algorithms', to: '/algorithms', position: 'left' },
-          { label: 'Tags', to: '/blog/tags', position: 'left' },
-          { label: 'Archive', to: '/blog/archive', position: 'left' },
+          { label: "Tags", to: "/blog/tags", position: "left" },
+          { label: "Archive", to: "/blog/archive", position: "left" },
           // {
           //   label: 'Playground',
           //   to: '/playground',
           //   position: 'left',
           // },
           {
-            label: 'GitHub',
-            href: 'https://github.com/seobie',
-            position: 'right',
+            label: "GitHub",
+            href: "https://github.com/seobie",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           // {
           //   title: 'Docs',
@@ -108,15 +109,15 @@ const config = {
           //   ],
           // },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/x.eob',
+                label: "Instagram",
+                href: "https://www.instagram.com/x.eob",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/seobie',
+                label: "GitHub",
+                href: "https://github.com/seobie",
               },
             ],
           },
@@ -124,8 +125,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Yeongseob Lee, Inc. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
       algolia: {
         // The application ID provided by Algolia
@@ -134,7 +135,7 @@ const config = {
         // Public API key: it is safe to commit it
         apiKey: process.env.ALGORIA_SEARCH_API_KEY,
 
-        indexName: 'seobie',
+        indexName: "seobie",
 
         // Optional: see doc section below
         contextualSearch: true,
@@ -146,7 +147,7 @@ const config = {
         // searchParameters: {},
 
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
+        searchPagePath: "search",
 
         //... other Algolia params
       },
@@ -185,28 +186,28 @@ const config = {
   </html>`,
   plugins: [
     [
-      '@docusaurus/plugin-content-blog',
+      "@docusaurus/plugin-content-blog",
       {
         /**
          * 멀티 인스턴스 플러그인 적용 시 필수값으로 설정해야 합니다.
          */
-        id: 'second-blog',
+        id: "second-blog",
         /**
          * 사이트에서 블로그 연결 시 사용할 URL 경로를 설정합니다.
          * *절대* URL 끝에 슬래시를 붙이지 마세요.
          */
-        routeBasePath: 'algorithms',
+        routeBasePath: "algorithms",
         /**
          * 사이트 디렉터리 기준으로 상대 경로를 지정합니다.
          */
-        path: './algorithms',
-        blogTitle: 'Algorithms',
-        blogDescription: '알고리즘 올리기 전용',
+        path: "./algorithms",
+        blogTitle: "Algorithms",
+        blogDescription: "알고리즘 올리기 전용",
         showReadingTime: true,
         // Please change this to your repo.
-        editUrl: 'https://github.com/seobie/seobie.github.io/tree/main/',
-        blogSidebarTitle: 'All posts',
-        blogSidebarCount: 'ALL',
+        editUrl: "https://github.com/seobie/seobie.github.io/tree/main/",
+        blogSidebarTitle: "All posts",
+        blogSidebarCount: "ALL",
         postsPerPage: 10,
       },
     ],
